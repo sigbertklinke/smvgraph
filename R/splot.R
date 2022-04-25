5ee25887c8fb6737c8eb5d7a3feffc1d651da7b5
Sigbert Klinke <sigbert.klinke@web.de>
2022-04-25 14:47:56 +0200

    vignette & small corrections

--- R/splot.R
+++ R/splot.R
@@ -13,14 +13,17 @@
 #' @examples
 #' if (interactive()) splot(iris)
 splot <- function(data, xvar=character(0), path=NULL) {
-  if (is.table(data) || stats::is.ts(data)) data <- toDataframe(data, paste(deparse(substitute(data), 500), collapse = "\n"))
-  stopifnot(is.data.frame(data))
-#  main <- paste(deparse(substitute(data), 500), collapse = "\n")
-  oldpar <- graphics::par(no.readonly = TRUE)
-  on.exit(resetpar(oldpar))
-  pkgs <- checkPackages()
-  if (!all(pkgs)) stop(sprintf("Package '%s' not installed", names(pkgs)[!pkgs]))
-  if (!is.null(path)) path <- tools::file_path_as_absolute(path)
-  shinyOptions('smvgraph.param'=list(file=toRDS(data), xvar=xvar, plotmodule='', path=path))
+  shinyOptions(smvgraph.param=NULL)
+  if (!missing(data)) {
+    if (is.table(data) || stats::is.ts(data)) data <- toDataframe(data, paste(deparse(substitute(data), 500), collapse = "\n"))
+    stopifnot(is.data.frame(data))
+    #  main <- paste(deparse(substitute(data), 500), collapse = "\n")
+    oldpar <- graphics::par(no.readonly = TRUE)
+    on.exit(resetpar(oldpar))
+    pkgs <- checkPackages()
+    if (!all(pkgs)) stop(sprintf("Package '%s' not installed", names(pkgs)[!pkgs]))
+    if (!is.null(path)) path <- tools::file_path_as_absolute(path)
+    shinyOptions(smvgraph.param=list(file=toRDS(data), xvar=xvar, plotmodule='', path=path))
+  } 
   source(system.file("app", "app.R", package = "smvgraph"), local = TRUE, chdir = TRUE)$value
 }
